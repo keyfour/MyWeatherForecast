5e446e8dfa8d8d565c1878a0507f3ada11ac76ef
Aleksandr Karpov <keyfour13@gmail.com>
2018-02-27 16:58:10 +0300

    Add interface for getting 5-days forecast

--- app/src/main/java/com/github/keyfour/myweatherforecast/data/OpenWeatherMapService.java
+++ app/src/main/java/com/github/keyfour/myweatherforecast/data/OpenWeatherMapService.java
@@ -4,8 +4,10 @@
 package com.github.keyfour.myweatherforecast.data;
 
 import com.github.keyfour.myweatherforecast.model.pojo.Forecast;
+import com.github.keyfour.myweatherforecast.model.pojo.ForecastList;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -20,6 +22,10 @@ public interface OpenWeatherMapService {
     @GET("/data/2.5/weather")
     Observable<Forecast> getForecast(@Query("lat") double lat, @Query("lon") double lon,
                                      @Query("APPID") String apiKey);
+
+    @GET("/data/2.5/forecast")
+    Observer<ForecastList> getFiveDaysForecast(@Query("lat") double lat, @Query("lon") double lon,
+                                               @Query("APPID") String apiKey);
 
 
 }
