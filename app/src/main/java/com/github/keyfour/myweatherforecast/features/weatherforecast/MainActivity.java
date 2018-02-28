75829f54e8eaf3a1d6ab67ff482a3ca666452501
Aleksandr Karpov <keyfour13@gmail.com>
2018-02-28 16:44:13 +0300

    Add user information to navigation header

--- app/src/main/java/com/github/keyfour/myweatherforecast/features/weatherforecast/MainActivity.java
+++ app/src/main/java/com/github/keyfour/myweatherforecast/features/weatherforecast/MainActivity.java
@@ -1,6 +1,7 @@
 package com.github.keyfour.myweatherforecast.features.weatherforecast;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -11,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.github.keyfour.myweatherforecast.R;
 import com.github.keyfour.myweatherforecast.features.login.LoginActivity;
@@ -18,10 +21,12 @@ import com.github.keyfour.myweatherforecast.features.settings.SettingFragment;
 import com.github.keyfour.myweatherforecast.features.weatherforecast.current.CurrentFragment;
 import com.github.keyfour.myweatherforecast.features.weatherforecast.fivedays.FivedaysFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,19 +44,44 @@ public class MainActivity extends AppCompatActivity
     @BindView(R.id.nav_view)
     NavigationView navigationView;
 
+
+    class NavViewFields {
+
+        @BindView(R.id.iv_user)
+        ImageView ivUser;
+
+        @BindView(R.id.tv_username)
+        TextView tvUsername;
+
+        @BindView(R.id.tv_email)
+        TextView tvEmail;
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        NavViewFields navViewFields = new NavViewFields();
+        ButterKnife.bind(navViewFields, navigationView.getHeaderView(0));
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            navViewFields.tvUsername.setText(account.getDisplayName());
+            navViewFields.tvEmail.setText(account.getEmail());
+            Uri user = account.getPhotoUrl();
+            Picasso.with(this).load(user).into(navViewFields.ivUser);
+        }
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.contentFrame,
